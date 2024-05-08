680077a36bde5cd8920c611222de09051f165bff
Dylan Pattipeilohy <dylan.pattipeilohy@hva.nl>
2024-05-08 15:38:25 +0200

    Added comments to methods and reorganized code

--- Assets/ProjectileScript.cs
+++ Assets/ProjectileScript.cs
@@ -4,46 +4,60 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
-    [SerializeField] 
-    private float speed = 10f; 
+    [SerializeField] private float speed = 10f; 
+    [SerializeField] private float maxDistance = 20f; 
+    [SerializeField] private float gravityScale = 1f; 
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float resetTime = 5f;
 
-    [SerializeField] 
-    private float maxDistance = 20f; 
-
-    [SerializeField] 
-    private float gravity = 9.81f; 
-
-    [SerializeField] 
-    private float damage = 10f;
-
-    [SerializeField] 
-    private float resetTime = 5f;
-
-    private float _distanceTraveled = 0f; 
+    private float _distanceTraveled; 
     private Vector3 _initialPosition;
     private Vector3 _lastPosition; 
     private Rigidbody2D _rb;
 
-    private void Start()
+    private void Awake()
     {
-        _initialPosition = transform.position;
         _rb = GetComponent<Rigidbody2D>();
+        _initialPosition = transform.position;
         _rb.velocity = transform.forward * speed;
         _rb.gravityScale = 0;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 displacement = transform.position - _lastPosition;
+        CalculateDistanceTraveled();
+        CalculateDeactivationTimer();
+    }
+
+    /// <summary>
+    /// Calculates the amount of distance the projectile has traveled, used to enable gravity after a certain distance
+    /// </summary>
+    private void CalculateDistanceTraveled()
+    {
+        var displacement = transform.position - _lastPosition;
         _distanceTraveled += displacement.magnitude;
 
         _lastPosition = transform.position;
 
         if (_distanceTraveled >= maxDistance)
         {
-            _rb.gravityScale = gravity;
+            EnablesGravity();
         }
+    }
 
+    /// <summary>
+    /// Enables gravity by specifying a value other than zero
+    /// </summary>
+    private void EnablesGravity()
+    {
+        _rb.gravityScale = gravityScale;
+    }
+
+    /// <summary>
+    /// Calculates the time before the objects gets deactavted due to inactivity (not Moving)
+    /// </summary>
+    private void CalculateDeactivationTimer()
+    {
         if (_rb.velocity.magnitude <= 0.1f)
         {
             resetTime -= Time.deltaTime;
@@ -58,12 +72,28 @@ public class ProjectileScript : MonoBehaviour
         }
     }
 
-  
+    /// <summary>
+    /// Resets the variables to their default and deactivates the gameObject
+    /// </summary>
     private void ResetAndDeactivate()
     {
         _distanceTraveled = 0f;
         _rb.velocity = Vector3.zero;
         transform.position = _initialPosition;
         gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        EnablesGravity();
+    }
+
+    /// <summary>
+    /// Launches the object towards a specified direction
+    /// </summary>
+    /// <param name="direction">The direction the projectile will be shot to</param>
+    public void Shoot(Vector2 direction)
+    {
+        _rb.AddForce(direction.normalized * speed);
     }
 }
