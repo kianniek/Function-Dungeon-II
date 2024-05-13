f87f5d38dd751a164d37dbdf5972ffb374a50c75
kian <kianniek@hotmail.com>
2024-05-13 15:45:32 +0200

    Added Feature: Trajectory Line #77

--- Assets/Scripts/LineController/FunctionLineController.cs
+++ Assets/Scripts/LineController/FunctionLineController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Attributes;
 using Projectile;
@@ -12,37 +13,37 @@ namespace LineController
     public class FunctionLineController : MonoBehaviour
     {
         [Header("Function settings")] public bool isQuadratic;
-        
-        [Header("Function coefficients")] 
-        [Tooltip("This variable defines the slope of the function.")] [SerializeField] private float a = 1f;
-        
-        [Tooltip("This variable defines the y-intercept of the function.")] 
+
+        [Header("Function coefficients")]
+        [Tooltip("This variable defines the slope of the function.")][SerializeField] private float a = 1f;
+
+        [Tooltip("This variable defines the y-intercept of the function.")]
         [SerializeField] private float b;
-        
-        [Tooltip("This variable defines the quadratic power of the function.")] 
-        [Min(0.001f)] 
+
+        [Tooltip("This variable defines the quadratic power of the function.")]
+        [Min(0.001f)]
         [SerializeField] private float c = 1f;
-        
+
         [FormerlySerializedAs("functionmLineLength")]
         [FormerlySerializedAs("lineLength")]
         [Header("Visual settings")]
         [SerializeField] private float functionLineLength = 10f;
         [SerializeField] private int segments = 10;
         [SerializeField] private LineRenderer lineRenderer;
-        
-        [Header("Trajectory Settings")] 
-        [Header("References")] 
+
+        [Header("Trajectory Settings")]
+        [Header("References")]
         [SerializeField] private LineRenderer trajectoryLineRenderer;
-        
-        [Header("Variables")] 
-        [Min(0.0001f)] 
+
+        [Header("Variables")]
+        [Min(0.0001f)]
         [SerializeField] private float timeStep = 0.1f; // Time step for calculating trajectory points
-        
+
         // [SerializeField] private float followDistance = 5f; // Distance along the function line to set the start point
         // [SerializeField] private float gravity = Physics.gravity.y; // Gravity value
         [SerializeField] private float groundLevel;
         [SerializeField, Expandable] private ProjectilePhysicsVariables projectilePhysicsVariables;
-        
+
         public float GroundLevel
         {
             get => groundLevel;
@@ -52,7 +53,7 @@ namespace LineController
                 CalculateTrajectory();
             }
         }
-        
+
         public float TimeStep
         {
             get => timeStep;
@@ -62,7 +63,7 @@ namespace LineController
                 CalculateTrajectory();
             }
         }
-        
+
         public float FunctionLineLength
         {
             get => functionLineLength;
@@ -72,7 +73,7 @@ namespace LineController
                 UpdateFunction(); // Update the line renderer whenever the value changes.
             }
         }
-        
+
         /// <summary>
         /// Sets the value of 'a' and updates the function.
         /// </summary>
@@ -85,7 +86,7 @@ namespace LineController
                 UpdateFunction();
             }
         }
-        
+
         /// <summary>
         /// Sets the value of 'b' and updates the function.
         /// </summary>
@@ -98,7 +99,7 @@ namespace LineController
                 UpdateFunction();
             }
         }
-        
+
         /// <summary>
         /// Sets the value of 'b' and updates the function.
         /// </summary>
@@ -111,12 +112,12 @@ namespace LineController
                 UpdateFunction();
             }
         }
-        
+
         private void Awake()
         {
             lineRenderer = GetComponent<LineRenderer>();
         }
-        
+
         /// <summary>
         /// Evaluates the function at a given 'x' value.
         /// </summary>
@@ -126,7 +127,7 @@ namespace LineController
         {
             return a * Mathf.Pow(x, c) + b;
         }
-        
+
         /// <summary>
         /// Computes the velocity at a point along the function.
         /// </summary>
@@ -135,7 +136,7 @@ namespace LineController
         {
             return new Vector2(1, a).normalized * projectilePhysicsVariables.Velocity;
         }
-        
+
         /// <summary>
         /// Draws the function line based on the current coefficients and settings.
         /// </summary>
@@ -145,25 +146,28 @@ namespace LineController
             {
                 c = 1; // Forcing 'c' to be 1 if not quadratic
             }
-            
+
             // Set the number of positions in the line renderer to match the number of segments and avoid exceeding the array bounds
             lineRenderer.positionCount = segments + 1;
-            
+
             var direction = new Vector2(1, EvaluateFunction(1));
             var lineEndPoint = ProjectilePhysics.CalculateGraphTrajectoryEndPoint(direction, FunctionLineLength);
-            
+
             // Iterate over each segment and evaluate the function
             for (var i = 0; i <= segments; i++)
             {
                 var point = Vector2.Lerp(Vector2.zero, lineEndPoint, i / (float)segments);
-                
+
                 // Set the position of the line renderer
                 lineRenderer.SetPosition(i, new(point.x, point.y, 0));
             }
-            
-            CalculateTrajectory();
+
+            if (trajectoryLineRenderer.enabled)
+            {
+                CalculateTrajectory();
+            }
         }
-        
+
         /// <summary>
         /// Calculates the trajectory of the projectile based on the function and physics variables.
         /// </summary>
@@ -171,24 +175,24 @@ namespace LineController
         {
             if (!trajectoryLineRenderer)
                 return;
-            
+
             // Calculate the direction and initial velocity of the projectile
             var direction = new Vector2(1, EvaluateFunction(1));
             var startPosition = CalculateStartPosition(direction);
             var initialVelocity = CalculateInitialVelocity(direction);
-            
+
             // Calculate the time it takes for the projectile to reach the ground
             var timeToGround = CalculateTimeToGround(startPosition.y, initialVelocity.y);
-            
+
             if (timeToGround < 0)
             {
                 Debug.LogWarning("Calculated negative time to ground, which is invalid.");
                 return;
             }
-            
+
             UpdateTrajectoryLineRenderer(startPosition, initialVelocity, timeToGround);
         }
-        
+
         /// <summary>
         /// Calculates the start position of the projectile based on the direction of the function.
         /// </summary>
@@ -200,7 +204,7 @@ namespace LineController
             var startPosition = ProjectilePhysics.CalculateGraphTrajectoryEndPoint(direction, clampedFollowDistance);
             return startPosition;
         }
-        
+
         /// <summary>
         /// Calculates the initial velocity of the projectile based on the direction of the function.
         /// </summary>
@@ -210,7 +214,7 @@ namespace LineController
         {
             return ProjectilePhysics.CalculateGravityTrajectory(direction, projectilePhysicsVariables.Velocity, projectilePhysicsVariables.Gravity);
         }
-        
+
         /// <summary>
         /// Calculates the time it takes for the projectile to hit the ground.
         /// </summary>
@@ -222,21 +226,21 @@ namespace LineController
             // Calculate the discriminant of the quadratic equation equation: b^2 - 4ac
             var discriminant = (initialVerticalVelocity * initialVerticalVelocity) -
                                4 * projectilePhysicsVariables.Gravity * (initialHeight - groundLevel);
-            
+
             if (discriminant < 0)
             {
                 Debug.LogWarning(
                     "No real solutions, projectile does not hit the ground in a normal parabolic trajectory.");
-                
+
                 return -1; // Indicates no real solution
             }
-            
+
             var timeToGround = (-initialVerticalVelocity - Mathf.Sqrt(discriminant)) /
                                -Mathf.Abs(projectilePhysicsVariables.Gravity);
-            
+
             return timeToGround;
         }
-        
+
         /// <summary>
         /// Updates the trajectory line renderer with the calculated trajectory points.
         /// </summary>
@@ -246,16 +250,16 @@ namespace LineController
         private void UpdateTrajectoryLineRenderer(Vector3 startPosition, Vector2 initialVelocity, float timeToGround)
         {
             //Debug.Log($"initialVelocity {initialVelocity}");
-            
+
             var calculatedSegments = Mathf.CeilToInt(timeToGround / timeStep);
             var trajectoryPoints = new List<Vector3>();
-            
+
             // Calculate the trajectory points
             for (var i = 0; i <= calculatedSegments; i++)
             {
                 // Calculate the displacement of the projectile
                 var time = i * timeStep;
-                
+
                 if (time > timeToGround)
                     time = timeToGround;
 
@@ -267,26 +271,42 @@ namespace LineController
 
                 // Calculate the next point along the trajectory
                 var nextPoint = startPosition + displacement;
-                
+
                 if (nextPoint.y < groundLevel)
                 {
                     nextPoint.y = groundLevel;
                     trajectoryPoints.Add(nextPoint);
                     break;
                 }
-                
+
                 trajectoryPoints.Add(nextPoint);
             }
-            
+
             trajectoryLineRenderer.positionCount = trajectoryPoints.Count;
             trajectoryLineRenderer.SetPositions(trajectoryPoints.ToArray());
         }
-        
+
+        /// <summary>
+        /// Sets the visibility of the trajectory line renderer.
+        /// </summary>
+        /// <param name="enable"></param>
+        public void TrajectoryLineIsVisible(bool enable)
+        {
+            trajectoryLineRenderer.enabled = enable;
+            StartCoroutine(EnableTrajectoryLineAfterTimeOut(5f));
+        }
+
+        private IEnumerator EnableTrajectoryLineAfterTimeOut(float time)
+        {
+            yield return new WaitForSeconds(time);
+            TrajectoryLineIsVisible(true);
+        }
+
         private void OnValidate()
         {
             if (projectilePhysicsVariables == null)
                 return;
-            
+
             UpdateFunction();
             CalculateTrajectory();
         }
