4818f84952d4f46a92e7284c0b03ddc1e8607694
winand <winand1990@live.nl>
2024-05-16 17:34:56 +0200

    Add a grade event listener

---
+++ Assets/Scripts/Progression/Grading/GradeGameEventListener.cs
@@ -0,0 +1,11 @@
+using GameEvent.Listeners;
+
+namespace Progression.Grading
+{
+    /// <summary>
+    /// Grade-typed <see cref="GameEventListener"/> to use when there is the need for passing through grade values.
+    /// </summary>
+    public class GradeGameEventListener : GameEventListenerBase<Grade, GradeGameEvent, GradeEvent>
+    {
+    }
+}
