5046024348bb77977c20c3da5c50c8c3516be0a8
Tigo <tigo.stam@hva.nl>
2024-05-27 14:52:02 +0200

    Smooshed 2 script together
    TextFollowLine is now inside LineToText

--- Assets/Scripts/Crafter/LineToText.cs
+++ Assets/Scripts/Crafter/LineToText.cs
@@ -5,15 +5,20 @@ namespace Crafter
 {
     public class LineToText : MonoBehaviour
     {
+        private LineRenderer _lineToFollow;
         private GameObject _line;
 
         private float _a;
         private float _b;
+        private int _indexOfPoint = 2;
+        private float _zPosition = -0.5f;
+        private float _yPositionOffset;
 
         private TextMeshPro _formulaText;
 
         private void Awake()
         {
+            _lineToFollow = GetComponentInParent<LineRenderer>();
             _line = transform.parent.gameObject;
             _formulaText = GetComponent<TextMeshPro>();
             _a = _line.GetComponent<FunctionLineController>().A;
@@ -22,7 +27,9 @@ namespace Crafter
 
         void Start()
         {
-            _formulaText.text = $"Y = {_a}x + {_b}";
+            _formulaText.text = $"Y = {_a}x + {_b}"; 
+            _yPositionOffset = transform.parent.position.y;
+            transform.position = new Vector3(_lineToFollow.GetPosition(_indexOfPoint).x, _lineToFollow.GetPosition(_indexOfPoint).y + _yPositionOffset, _zPosition);
         }
     }
 }
