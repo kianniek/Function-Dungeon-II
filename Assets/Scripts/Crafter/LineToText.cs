d5d32e3aab06ddb2212fd3ebcc415a1c92a031a3
Winand Metz <54841668+MisakiCopperrose@users.noreply.github.com>
2024-05-30 14:21:01 +0200

    Update LineToText.cs
    Cleanup & formatting

--- Assets/Scripts/Crafter/LineToText.cs
+++ Assets/Scripts/Crafter/LineToText.cs
@@ -5,17 +5,15 @@ namespace Crafter
 {
     public class LineToText : MonoBehaviour
     {
+        private const float _zPosition = -0.5f;
+        
         [SerializeField] private LineRenderer lineToFollow;
-
         [SerializeField] private int indexOfPointToFollow = 2;
 
         private Transform _line;
-
         private TextMeshPro _formulaText;
-
         private float _a;
         private float _b;
-        const float _zPosition = -0.5f;
         private float _yPositionOffset;
 
         private void Awake()
@@ -26,11 +24,16 @@ namespace Crafter
             _b = _line.position.y;
         }
 
-        void Start()
+        private void Start()
         {
             _formulaText.text = $"Y = {_a}x + {_b}"; 
             _yPositionOffset = transform.parent.position.y;
-            transform.position = new Vector3(lineToFollow.GetPosition(indexOfPointToFollow).x, lineToFollow.GetPosition(indexOfPointToFollow).y + _yPositionOffset, _zPosition);
+            
+            transform.position = new Vector3(
+                lineToFollow.GetPosition(indexOfPointToFollow).x, 
+                lineToFollow.GetPosition(indexOfPointToFollow).y + _yPositionOffset, 
+                _zPosition
+            );
         }
     }
 }
