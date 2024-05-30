dcc3b27bb1861df485581475db039badf18c35ef
Winand Metz <54841668+MisakiCopperrose@users.noreply.github.com>
2024-05-30 14:30:47 +0200

    Update StringManipulation.cs

--- Assets/Scripts/StringManipulation.cs
+++ Assets/Scripts/StringManipulation.cs
@@ -6,7 +6,7 @@ public static class StringManipulation
     /// Only leaves numbers, ',' and '.' in a string
     /// </summary>
     /// <param name="input">Input string</param>
-    /// <returns></returns>
+    /// <returns>Input string with just the numbers and decimal indication left</returns>
     public static string CleanUpDecimalOnlyString(string input)
     {
         return Regex.Replace(input, @"[^0-9.,]+", "");
