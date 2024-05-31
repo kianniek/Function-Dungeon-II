433aaf7328b873e4e4afecd0b60a8c1faf2d8753
Tigo <tigo.stam@hva.nl>
2024-05-31 11:18:25 +0200

    Update PathTile.cs

--- Assets/Scripts/WorldGrid/PathTile.cs
+++ Assets/Scripts/WorldGrid/PathTile.cs
@@ -4,16 +4,6 @@ namespace WorldGrid
 {
     public class PathTile : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
 
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
