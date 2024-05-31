0429719bd9c852c112cb47ff8581a0b94d5c1df2
Tigo <tigo.stam@hva.nl>
2024-05-31 11:13:54 +0200

    Improved code quality

--- Assets/Scripts/WorldGrid/GridGenerator.cs
+++ Assets/Scripts/WorldGrid/GridGenerator.cs
@@ -10,41 +10,46 @@ namespace WorldGrid
         [SerializeField] private GameObject gridTile;
         [SerializeField] private int xSize = 20;
         [SerializeField] private int ySize = 20;
-        [SerializeField] private bool generate;
         [SerializeField] private Material pathMaterial;
         [SerializeField] private Material availableMaterial;
         [SerializeField] private PathData pathData;
 
+        private Vector2[] _directions = { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
+
         private void Start()
         {
-            if (generate)
+            for (var i = 0; i < xSize; i++)
             {
-                for (var i = 0; i < xSize; i++)
+                for (var j = 0; j < ySize; j++)
                 {
-                    for (var j = 0; j < ySize; j++)
-                    {
-                        GameObject tile = Instantiate(gridTile, new Vector3(i, j, 0), Quaternion.identity, transform);
-                        tile.name = $"GridTile({i},{j})";
+                    GameObject tile = Instantiate(gridTile, new Vector3(i, j, 0), Quaternion.identity, transform);
+                    tile.name = $"GridTile({i},{j})";
 
-                        for (var k = 0; k < pathData.PathCoordinates.Count; k++)
+                    for (var k = 0; k < pathData.PathCoordinates.Count; k++)
+                    {
+                        var currentTile = new Vector2(i, j);
+                        if (pathData.PathCoordinates[k] == currentTile)
                         {
-                            var currentPathTile = new Vector2(i, j);
-                            if (pathData.PathCoordinates[k] == currentPathTile)
-                            {
-                                tile.AddComponent<PathTile>();
-                                tile.GetComponent<SpriteRenderer>().material = pathMaterial;
-                            }
-                            CheckNeighbourTiles(pathData.PathCoordinates[k] + new Vector2(-1, 0), currentPathTile, tile);
-                            CheckNeighbourTiles(pathData.PathCoordinates[k] + new Vector2(+1, 0), currentPathTile, tile);
-                            CheckNeighbourTiles(pathData.PathCoordinates[k] + new Vector2(0, -1), currentPathTile, tile);
-                            CheckNeighbourTiles(pathData.PathCoordinates[k] + new Vector2(0, +1), currentPathTile, tile);
+                            tile.AddComponent<PathTile>();
+                            tile.GetComponent<SpriteRenderer>().material = pathMaterial;
+                        }
+
+                        foreach (var direction in _directions)
+                        {
+                            IsPlaceableTile(pathData.PathCoordinates[k] + direction, currentTile, tile);
                         }
                     }
                 }
             }
         }
 
-        private void CheckNeighbourTiles(Vector2 pathTileToCheck, Vector2 currentPathTile, GameObject tile)
+        /// <summary>
+        /// Checks if currentTile is an placeable tile by checking if the cell is next to an path and if its not an path
+        /// </summary>
+        /// <param name="pathTileToCheck">The pathcell you want to check</param>
+        /// <param name="currentPathTile">The current path tile of the grid</param>
+        /// <param name="tile">The current path tile gameobject</param>
+        private void IsPlaceableTile(Vector2 pathTileToCheck, Vector2 currentPathTile, GameObject tile)
         {
             if (pathTileToCheck == currentPathTile)
             {
