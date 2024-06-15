2c0bc5ca9328ecbe17ff7b36c447ff0ccad370c7
winand <winand1990@live.nl>
2024-06-15 12:29:24 +0200

    Fix namespaces & imports

--- Assets/Scripts/UI/LinearEquation/LinearEquationFormulaText.cs
+++ Assets/Scripts/UI/LinearEquation/LinearEquationFormulaText.cs
@@ -1,63 +1,65 @@
-using System;
 using LinearFunction;
 using TMPro;
 using UnityEngine;
 
-/// <summary>
-/// Linear equation formula text class that displays the linear equation formula
-/// </summary>
-public class LinearEquationFormulaText : MonoBehaviour
+namespace UI.LinearEquation
 {
-    [SerializeField] private LinearFunctionData linearFunctionData;
-    [SerializeField] private bool setTextOnStart = true;
-    
-    private const string TextFormat = "y = {0}x + {1}";
-    private const string GenericTextFormat = "y = ax + b";
-    
-    private TMP_Text _text;
-    private string outputText;
-    private float _slope;
-    private float _yIntercept;
-    private bool _useGenericTextFormat;
-    
     /// <summary>
-    /// Gets or sets the use generic text format
+    /// Linear equation formula text class that displays the linear equation formula
     /// </summary>
-    public bool UseGenericTextFormat
+    public class LinearEquationFormulaText : MonoBehaviour
     {
-        get => _useGenericTextFormat;
-        set
+        [SerializeField] private LinearFunctionData linearFunctionData;
+        [SerializeField] private bool setTextOnStart = true;
+    
+        private const string TextFormat = "y = {0}x + {1}";
+        private const string GenericTextFormat = "y = ax + b";
+    
+        private TMP_Text _text;
+        private string outputText;
+        private float _slope;
+        private float _yIntercept;
+        private bool _useGenericTextFormat;
+    
+        /// <summary>
+        /// Gets or sets the use generic text format
+        /// </summary>
+        public bool UseGenericTextFormat
         {
-            _useGenericTextFormat = value;
+            get => _useGenericTextFormat;
+            set
+            {
+                _useGenericTextFormat = value;
+                SetText();
+            }
+        }
+    
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+    
+        private void Start()
+        {
+            if (setTextOnStart)
+                PullLinearFunctionData();
+        }
+    
+        // Set the text
+        private void SetText()
+        {
+            outputText = UseGenericTextFormat ? GenericTextFormat : TextFormat;
+            _text.text = string.Format(outputText, _slope, _yIntercept);
+        }
+    
+        /// <summary>
+        /// Pulls the linear function data from the LinearFunctionData scriptable object
+        /// </summary>
+        public void PullLinearFunctionData()
+        {
+            _slope = linearFunctionData.Slope;
+            _yIntercept = linearFunctionData.YIntercept;
             SetText();
         }
-    }
-    
-    private void Awake()
-    {
-        _text = GetComponent<TMP_Text>();
-    }
-    
-    private void Start()
-    {
-        if (setTextOnStart)
-            PullLinearFunctionData();
-    }
-    
-    // Set the text
-    private void SetText()
-    {
-        outputText = UseGenericTextFormat ? GenericTextFormat : TextFormat;
-        _text.text = string.Format(outputText, _slope, _yIntercept);
-    }
-    
-    /// <summary>
-    /// Pulls the linear function data from the LinearFunctionData scriptable object
-    /// </summary>
-    public void PullLinearFunctionData()
-    {
-        _slope = linearFunctionData.Slope;
-        _yIntercept = linearFunctionData.YIntercept;
-        SetText();
     }
 }
